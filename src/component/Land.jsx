8261e3693872e779d177759bcd7628afb33a83a3
unknown <dangabarin2020@gmail.com>
2026-08-09 17:29:41 +0100

    update commit

--- src/component/Land.jsx
+++ src/component/Land.jsx
@@ -18,22 +18,33 @@ function Land({preloaderOut}) {
    const yMovement=useTransform(y,v=>v*.012) 
   return (
     <div ref={landingRef} className='relative h-svh w-full overflow-hidden bg-[#262626]'>
+      
         <img src="/assets/car-background.png" alt="Land" className="absolute top-0 left-0 w-full h-full object-cover"/>
+        <img src="/assets/car-background-mobile.png" alt="Land" className="aspect-[5/7] inline lg:hidden absolute top-0 left-0 w-full h-full object-cover"/>
   <MarqueeX numbers={6} speed={180}  className='absolute hidden lg:flex  top-0 left-0  w-full h-full  border-none bg-gree-600' >
   <h2 className=" w-full h-full fl gap-4  font-custom text-[#FCCE2F] mr-[12em] font-bod">
     <BrandName/> 
      </h2>
   </MarqueeX>
 
-  <motion.div   style={{x:newX,y:newY}} className="absolute top-0 left-0 w-full h-full">
+  <motion.div   style={{x:newX,y:newY}} className=" lg:inline hidden absolute top-0 left-0 w-full h-full">
         <motion.img
-        
         animate={{x:preloaderOut?0:300,y:preloaderOut?0:-20,scale:preloaderOut?1:.95}}
         transition={{type:"tween",duration:1.8,ease:[0.22, 1, 0.36, 1]}}
           initial={{x:300,y:-20,scale:.95}}
           exit={{x:300,y:-20,scale:.95}}
           // animate={{x:preloaderOut?newX:300,y:preloaderOut?0:-20,scale:preloaderOut?1:.95}}
-           src="/assets/car-alone.png" alt="Land" className=" object-cover"/>
+           src="/assets/car-alone.png" alt="Land" className="absolute top-0 left-0 w-full h-full object-cover"/>
+
+  </motion.div>
+  <motion.div   style={{x:newX,y:newY}} className=" aspect-[5/7] inline lg:hidden absolute top-0 left-0 w-full h-full">
+        <motion.img
+        animate={{x:preloaderOut?0:300,y:preloaderOut?0:-20,scale:preloaderOut?1:.95}}
+        transition={{type:"tween",duration:1.8,ease:[0.22, 1, 0.36, 1]}}
+          initial={{x:300,y:-20,scale:.95}}
+          exit={{x:300,y:-20,scale:.95}}
+          // animate={{x:preloaderOut?newX:300,y:preloaderOut?0:-20,scale:preloaderOut?1:.95}}
+           src="/assets/car-alone-mobile.png" alt="Land" className="absolute top-0 left-0 w-full h-full object-cover"/>
 
   </motion.div>
 
