a8dddfe3978d92c6ea1223ddc5b2b9958471f425
unknown <dangabarin2020@gmail.com>
2026-08-12 16:02:27 +0100

    update commit

--- src/component/Section1.jsx
+++ src/component/Section1.jsx
@@ -3,14 +3,15 @@ import Section from '@/layout/Section'
 import React from 'react'
 import TypographyReveal from './TextReveals'
 import AnimatedArrow from './AnimatedArrow'
+import Image from 'next/image'
 
 function Section1() {
   return (
-    <div className='h-svh'>
+    <div className='min-h-[85svh]'>
         
         <Section>
-          <GridColumn>
-            <div className="col-span-3 bg-red400">
+          <GridColumn gridLines className={"h-full"}>
+            <div className="col-span-3 lg:col-span-3 bg-red400">
               <h2 className='text-brand-accent text-heading2 leading-[.8] font-custom'>
                   <TypographyReveal className='font-custom capitalie leading-[1.1]'
     toColor='#0100be'
@@ -25,7 +26,7 @@ function Section1() {
               <AnimatedArrow className='w-10 rotate-[100deg] lg:rotate-[40deg] '/>
               {/* <h2 className='text-brand-accent text-sm font-body'>what we do</h2> */}
             </div>
-            <div className="col-span-7">
+            <div className="lg:col-start-6 lg:col-span-7 col-span-full">
             <h1 className='capitalie'>
    <TypographyReveal className='font-custom text-brand-accent text-heading2 capitalie leading-[1.1]'
     toColor='#0100be'
@@ -37,6 +38,25 @@ Expert Car Detaiing: from luxury brands to your everyday ride in Miami
             </h1>
 
             </div>
+         <div className="col-span-full lg:col-start-1 lg:col-span-5 mt-10">
+  <Image
+    src="/assets/red-benz.png"
+    alt="hero"
+    width={1000}
+    height={1000}
+    className="w-full h-auto object-cover rounded-2xl"
+  />
+</div>
+
+<div className="col-span-full lg:col-start-10 lg:col-span-3  place-items-end grid">
+  <Image
+    src="/assets/red-benz.png"
+    alt="hero"
+    width={1000}
+    height={1000}
+    className="w-full h-auto object-cover rounded-2xl"
+  />
+</div>
           </GridColumn>
         </Section>
         
